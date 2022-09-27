c71fadbbc0f646b0529b349b0adfa98f327a3e69
letChristianSosa <chrissm28@outlook.com>
2022-09-27 02:17:01 -0500

    Inicio hasta video 163

---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Layout from "./components/Layout";
+import NuevoCliente from "./pages/NuevoCliente";
+import Index, { loader as clientesLoader } from "./pages/Index";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Index />,
+        // Se asigna el loader que se va a ejecutar al renderizar el componente
+        loader: clientesLoader,
+      },
+      {
+        path: "/clientes/nuevo",
+        element: <NuevoCliente />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
